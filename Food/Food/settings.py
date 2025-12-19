1623e74692fc8d32a792d3ca8817031f75f9eca8
KATAKYIE-KWESI <qwesimike9@gmail.com>
2025-12-19 03:19:59 +0000

    Creating tables

--- Food/Food/settings.py
+++ Food/Food/settings.py
@@ -86,16 +86,23 @@ CSRF_TRUSTED_ORIGINS = [
 
 
 # --------------------------
-# Database
+# Databases
 # --------------------------
-DATABASES = {
-    'default': dj_database_url.config(
-        default=os.environ.get("DATABASE_URL"),
-        conn_max_age=600,
-        ssl_require=True
-    )
-}
-
+if os.environ.get('DATABASE_URL'):
+    DATABASES = {
+        'default': dj_database_url.config(
+            default=os.environ.get("DATABASE_URL"),
+            conn_max_age=600,
+            ssl_require=True
+        )
+    }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 # --------------------------
 # Password Validators
 # --------------------------
